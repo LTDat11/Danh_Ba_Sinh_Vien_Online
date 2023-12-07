7c9339d4c99c3d46c6d0776ff6934e8aeb06e17e
LTDat11 <ludat2410@gmail.com>
2023-12-07 13:38:54 +0700

    UI add info, check date < 18

--- app/src/main/java/com/example/myapplication/Fragments/AddFragment.kt
+++ app/src/main/java/com/example/myapplication/Fragments/AddFragment.kt
@@ -1,12 +1,16 @@
 package com.example.myapplication.Fragments
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.myapplication.R
 import com.example.myapplication.databinding.FragmentAddBinding
+import java.time.LocalDate
+import java.util.Calendar
 
 class AddFragment : Fragment() {
     lateinit var binding: FragmentAddBinding
@@ -19,6 +23,34 @@ class AddFragment : Fragment() {
 
         binding.apply {
 
+            btnDate.setOnClickListener {
+                val currentDate = Calendar.getInstance()
+                val year = currentDate.get(Calendar.YEAR)
+                val month = currentDate.get(Calendar.MONTH)
+                val day = currentDate.get(Calendar.DAY_OF_MONTH)
+
+                val datePickerDialog = DatePickerDialog(requireContext(), DatePickerDialog.OnDateSetListener { _, yearSelected, monthOfYear, dayOfMonth ->
+                    // Tạo một Calendar với ngày được chọn
+                    val selectedDate = Calendar.getInstance()
+                    selectedDate.set(yearSelected, monthOfYear, dayOfMonth)
+
+                    // Tính toán sự chênh lệch giữa ngày hiện tại và ngày được chọn
+                    val ageDifference = currentDate.get(Calendar.YEAR) - yearSelected
+
+                    // Kiểm tra điều kiện nếu nhỏ hơn 18 tuổi
+                    if (ageDifference < 18) {
+                        // Hiển thị thông báo Toast
+                        Toast.makeText(requireContext(), "Bạn chưa đủ 18 tuổi", Toast.LENGTH_SHORT).show()
+                    } else {
+                        // Hiển thị ngày đã chọn lên TextView
+                        tvDateNow.text = "$dayOfMonth/${monthOfYear + 1}/$yearSelected"
+                    }
+                }, year, month, day)
+
+                // Hiển thị DatePickerDialog
+                datePickerDialog.show()
+
+            }
         }
 
         // Inflate the layout for this fragment
--- app/src/main/java/com/example/myapplication/Fragments/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/Fragments/HomeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.example.myapplication.R
 import com.example.myapplication.databinding.FragmentHomeBinding
 
 
@@ -18,15 +20,26 @@ class HomeFragment : Fragment() {
 
         binding.apply {
 
+            setupSpinner()
+
             materialCardView.setOnClickListener {
                 searchview.isIconified = false
             }
+
+
 
 
         }
 
         // Inflate the layout for this fragment
         return binding.root
+    }
+
+    private fun setupSpinner() {
+        val list = resources.getStringArray(R.array.fillter)
+        val adapter = ArrayAdapter(requireContext(),android.R.layout.simple_spinner_item,list)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spinnerFillter.adapter = adapter
     }
 
 }
